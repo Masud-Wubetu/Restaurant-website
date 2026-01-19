e539cff31cdeebd3850566dbca54b2bf57234e41
Masud <wubetumasud@gmail.com>
2026-01-19 22:33:17 +0300

    Menu model

---
+++ backend/src/models/menuItem.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  category: String,
+  isAvailable: {
+    type: Boolean,
+    default: true
+  }
+});
+
+module.exports = mongoose.model('MenuItem', menuItemSchema);
