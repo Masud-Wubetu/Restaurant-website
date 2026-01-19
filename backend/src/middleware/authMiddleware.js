9e416e93a47edfb4a4180dd0b55c9231c141656c
Masud <wubetumasud@gmail.com>
2026-01-19 22:31:24 +0300

    Auth Middleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+exports.protect = (req, res, next) => {
+    let token = req.headers.authorization;
+
+    if(!token || !token.startsWith('Bearer')) {
+         return res.status(401).json({ message: 'Not authorized' });
+    }
+
+    token = token.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Token invalid' });
+    }
+};
+
+exports.adminOnly = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Admin access only' });
+    }
+    next();
+};
+
