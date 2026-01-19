ddf51f0427b58c1b6ab0d4f695cab8a5d78378f0
Masud <wubetumasud@gmail.com>
2026-01-19 22:42:00 +0300

    menu Contoller

--- backend/src/controllers/menuController.js
+++ backend/src/controllers/menuController.js
@@ -4,3 +4,22 @@ exports.createItem = async (req, res) => {
   const item = await MenuItem.create(req.body);
   res.status(201).json(item);
 };
+
+exports.getMenu = async (req, res) => {
+  const menu = await MenuItem.find({ isAvailable: true });
+  res.json(menu);
+};
+
+exports.updateItem = async (req, res) => {
+  const item = await MenuItem.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(item);
+};
+
+exports.deleteItem = async (req, res) => {
+  await MenuItem.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Item deleted' });
+};
